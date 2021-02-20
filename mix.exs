e390a38871379ed7c752c6051bad4076042c702f
Justin Smestad <justin.smestad@gmail.com>
2021-02-20 16:45:01 -0700

    Update mix.exs file

--- mix.exs
+++ mix.exs
@@ -5,9 +5,21 @@ defmodule Epoch.MixProject do
     [
       app: :epoch,
       version: "1.0.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "A library that converts between epoch and native Elixir DateTime",
+      package: package(),
+      source_url: "https://github.com/jsmestad/epoch.ex"
+    ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Justin Smestad"],
+      licenses: ["Apache-2.0"],
+      links: %{"Github" => "https://github.com/jsmestad/epoch.ex"}
     ]
   end
 
@@ -18,6 +30,8 @@ defmodule Epoch.MixProject do
   end
 
   defp deps do
-    []
+    [
+      {:ex_doc, ">= 0.19.0", only: :dev}
+    ]
   end
 end
